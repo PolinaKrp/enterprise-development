acc00f3bf003927e92a372412a2eacd37f5460a2
Polina <114875941+PolinaKrp@users.noreply.github.com>
2024-12-22 16:13:53 +0400

    добавила методы в враппер

--- HRDepartment.Domain/HRDepartment.WebApplication/Api/HRDepartmentApiWrapper.cs
+++ HRDepartment.Domain/HRDepartment.WebApplication/Api/HRDepartmentApiWrapper.cs
@@ -1,8 +1,9 @@
 ﻿using HRDepartment.WebApplication;
+using HRDepartment.WebApplication.Api;
 
 namespace BenefitTypeBooking.Client.API;
 
-public class HRDepartmentApiWrapper(IConfiguration configuration) 
+public class HRDepartmentApiWrapper(IConfiguration configuration)
 {
     public readonly HRDepartmentApi client = new(configuration["OpenApi:ServerUrl"], new HttpClient());
 
@@ -15,4 +16,34 @@ public class HRDepartmentApiWrapper(IConfiguration configuration)
     public async Task UpdateBenefitType(int id, BenefitTypePostDto BenefitTypePostDto) => await client.BenefitTypePUTAsync(id, BenefitTypePostDto);
 
     public async Task DeleteBenefitType(int id) => await client.BenefitTypeDELETEAsync(id);
+
+    public async Task CreateEmployee(EmployeePostDto EmployeePostDto) => await client.EmployeePOSTAsync(EmployeePostDto);
+
+    public async Task<ICollection<EmployeeGetDto>> GetAllEmployees() => await client.EmployeeAllAsync();
+
+    public async Task<EmployeeGetDto> GetEmployeeById(int id) => await client.EmployeeGETAsync(id);
+
+    public async Task UpdateEmployee(int id, EmployeePostDto EmployeePostDto) => await client.EmployeePUTAsync(id, EmployeePostDto);
+
+    public async Task DeleteEmployee(int id) => await client.EmployeeDELETEAsync(id);
+
+    public async Task CreateDepartment(DepartmentPostDto DepartmentPostDto) => await client.DepartmentPOSTAsync(DepartmentPostDto);
+
+    public async Task<ICollection<DepartmentGetDto>> GetAllDepartments() => await client.DepartmentAllAsync();
+
+    public async Task<DepartmentGetDto> GetDepartmentById(int id) => await client.DepartmentGETAsync(id);
+
+    public async Task UpdateDepartment(int id, DepartmentPostDto DepartmentPostDto) => await client.DepartmentPUTAsync(id, DepartmentPostDto);
+
+    public async Task DeleteDepartment(int id) => await client.DepartmentDELETEAsync(id);
+
+    public async Task CreateWorkshop(WorkshopPostDto WorkshopPostDto) => await client.WorkshopPOSTAsync(WorkshopPostDto);
+
+    public async Task<ICollection<WorkshopGetDto>> GetAllWorkshops() => await client.WorkshopAllAsync();
+
+    public async Task<WorkshopGetDto> GetWorkshopById(int id) => await client.WorkshopGETAsync(id);
+
+    public async Task UpdateWorkshop(int id, WorkshopPostDto WorkshopPostDto) => await client.WorkshopPOSTAsync(WorkshopPostDto);
+
+    public async Task DeleteWorkshop(int id) => await client.WorkshopDELETEAsync(id);
 }
--- HRDepartment.Domain/HRDepartment.WebApplication/Api/IHRDepartmentApiWrapper.cs
+++ HRDepartment.Domain/HRDepartment.WebApplication/Api/IHRDepartmentApiWrapper.cs
@@ -2,10 +2,39 @@
 {
     public interface IHRDepartmentApiWrapper
     {
-        Task CreateBenefitType(BenefitTypeGetDto BenefitTypeDto);
+        // Методы для работы с типами льгот
+        Task CreateBenefitType(BenefitTypePostDto benefitTypeDto);
         Task<ICollection<BenefitTypeGetDto>> GetAllBenefitTypes();
         Task<BenefitTypeGetDto> GetBenefitTypeById(int id);
-        Task UpdateBenefitType(int id, BenefitTypeGetDto BenefitTypeDto);
+        Task UpdateBenefitType(int id, BenefitTypePostDto benefitTypeDto);
         Task DeleteBenefitType(int id);
+
+        // Методы для работы с сотрудниками
+        Task CreateEmployee(EmployeePostDto employeePostDto);
+        Task<ICollection<EmployeeGetDto>> GetAllEmployees();
+        Task<EmployeeGetDto> GetEmployeeById(int id);
+        Task UpdateEmployee(int id, EmployeePostDto employeePostDto);
+        Task DeleteEmployee(int id);
+
+        // Методы для работы с отделами
+        Task CreateDepartment(DepartmentPostDto departmentPostDto);
+        Task<ICollection<DepartmentGetDto>> GetAllDepartments();
+        Task<DepartmentGetDto> GetDepartmentById(int id);
+        Task UpdateDepartment(int id, DepartmentPostDto departmentPostDto);
+        Task DeleteDepartment(int id);
+
+        // Методы для работы с позициями
+        Task CreatePosition(PositionPostDto positionPostDto);
+        Task<ICollection<PositionGetDto>> GetAllPositions();
+        Task<PositionGetDto> GetPositionById(int id);
+        Task UpdatePosition(int id, PositionPostDto positionPostDto);
+        Task DeletePosition(int id);
+
+        // Методы для работы с цехами
+        Task CreateWorkshop(WorkshopPostDto workshopPostDto);
+        Task<ICollection<WorkshopGetDto>> GetAllWorkshops();
+        Task<WorkshopGetDto> GetWorkshopById(int id);
+        Task UpdateWorkshop(int id, WorkshopPostDto workshopPostDto);
+        Task DeleteWorkshop(int id);
     }
 }
